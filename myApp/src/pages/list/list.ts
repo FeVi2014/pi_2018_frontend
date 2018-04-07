e10cf90fae05876ee9ccb6d2cfdb3caa86d9630c
FeVi2014 <31832058+FeVi2014@users.noreply.github.com>
2018-04-07 17:43:04 -0300

    feature/ionic start

---
+++ myApp/src/pages/list/list.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['football'];
+
+    this.items = [];
+    for (let i = 1; i < 5; i++) {
+      this.items.push({
+        title: 'Time' + i,
+        note: 'Esse é o time #' + i,
+        icon: this.icons[this.icons.length-1]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(ListPage, {
+      item: item
+    });
+  }
+}
