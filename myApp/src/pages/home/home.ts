f453be6723707369ae22e2abaa008abbfd7435fc
FeVi2014 <31832058+FeVi2014@users.noreply.github.com>
2018-04-07 20:13:04 -0300

    prototipo home

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -6,9 +6,30 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  lastGame: Object;
+  lastestPosts: Array<Object>;
+  
   constructor(public navCtrl: NavController) {
-
+    this.lastGame = {
+      winner:{
+        name: "Alemanha",
+        score: "7",
+        logo: '../assets/imgs/alemanha.png'
+      },
+      looser:{
+        name: "Brasil",
+        score: "1",
+        logo: '../assets/imgs/brasil.png'
+      },
+      date: "05/02/2018"
+    }
+    this.lastestPosts = [];
+    for (let i = 1; i < 5; i++) {
+      this.lastestPosts.push({
+        title: 'Que Jogo bosta',
+        comment: 'Nunca mais vejo futebol',
+        icon: 'assets/imgs/logo.png'
+      });
+    }
   }
-
 }
