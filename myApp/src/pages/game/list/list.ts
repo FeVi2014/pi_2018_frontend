7903e07124d76d4ae7f8d9698809f34f0a5ae45b
FeVi2014 <31832058+FeVi2014@users.noreply.github.com>
2018-04-08 16:09:15 -0300

    ajustes tela de lista de jogos

--- myApp/src/pages/game/list/list.ts
+++ myApp/src/pages/game/list/list.ts
@@ -10,11 +10,17 @@ import { FormPage as GameForm } from '../form/form';
 })
 export class ListPage {
 
-  championship: Object
+  championship: Array<{
+      id:number,
+      year: number,
+      league: Array<object>
+    }>;
   league: string;
+  champion: number;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+    
     const games = [
       {
         id:1,
@@ -60,34 +66,98 @@ export class ListPage {
         },
         date: "03/02/2018",
         time: "09:30"
+      },
+      {
+        id:4,
+        winner:{
+          name: "Brasil",
+          score: 8,
+          logo: '../assets/imgs/brasil.png'
+        },
+        looser:{
+          name: "Austrália",
+          score: 3,
+          logo: '../assets/imgs/australia.png'
+        },
+        date: "02/02/2018",
+        time: "10:57"
       }
     ];
 
-    this.championship = {
-      league:[
-        {
-          title:"master",
-          shift:[
-            {
-              round: [games]
-            }
-          ]
-        },
-        {
-          title:"senior",
-          shift:[
-            {
-              round: [games]
-            }
-          ]
-        },
-      ],
-    }
+    const shifts = [
+      {
+        title:"Semifinais",
+        round: {
+          games:[games,games,games,games],
+          number: 4
+        }
+      },
+      {
+        title:"Quartas",
+        round: {
+          games:[games,games,games,games],
+          number: 3
+        }
+      },
+      {
+        title:"2º Turno",
+        round: {
+          games:[games,games,games,games],
+          number: 2
+        }
+      },
+      {
+        title:"1º Turno",
+        round: {
+          games:[games,games,games,games],
+          number: 1
+        }
+      },
+    ]
+
+    this.championship = [
+      {
+        id:1,
+        year: 2018,
+        league:[
+          {
+            title:"master",
+            shift: shifts
+          },
+          {
+            title:"senior",
+            shift: shifts
+          },
+        ],
+      },
+      {
+        id:2,
+        year: 2017,
+        league:[
+          {
+            title:"master",
+            shift: shifts
+          },
+          {
+            title:"senior",
+            shift: shifts
+          },
+        ],
+      }
+    ]
 
     //precisar ser dinamico
     this.league = "master";
+    
+    this.champion = this.getLatest();
+    
   }
-
+ getLatest():number {
+    this.championship.map(o => {
+      if(o.year ==  new Date().getFullYear()) return o.id;
+    })
+    return 1;
+  }
   toGameDetails(id) {
     this.navCtrl.push(GameDetails, { id: id });
   }
